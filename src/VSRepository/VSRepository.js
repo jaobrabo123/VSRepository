3f7af39a6d05aa5cb2184bb192e641726b6a0109
jaobrabo123 <jp2904.lindo@gmail.com>
2026-05-04 00:47:56 -0300

    Separando classes de erro

--- src/VSRepository/VSRepository.js
+++ src/VSRepository/VSRepository.js
@@ -5,39 +5,11 @@
 * O código não tá muito organizado, dá pra melhorar muito, mas já ta funcionando bunitin
 */
 
-// * Essa é a classe de erro base, pode ser usada em debug para saber se um erro lançado veio do VSRepository
-export class VSRepoError extends Error {
-    constructor(message, type){
-        super(message);
-        this.name = this.constructor.name;
-        this.type = type;
-    }
-}
+// * Importando classes de erro
+import { VSRepoConfigError, VSRepoBuildError, VSRepoExtendError, VSRepoRuntimeError, VSRepoError } from "./VSRepoError.js";
 
-// * Dessa em diante são classes de erros específicas que estendem da base para saber em qual estágio do VSRepository foi lançado o erro, não vou detalhar cada uma pq o nome é auto explicativo
-export class VSRepoConfigError extends VSRepoError {
-    constructor(message){
-        super(message, "VSREPO_CONFIG");
-    }
-}
-
-export class VSRepoBuildError extends VSRepoError {
-    constructor(message){
-        super(message, "VSREPO_BUILD");
-    }
-}
-
-export class VSRepoExtendError extends VSRepoError {
-    constructor(message){
-        super(message, "VSREPO_EXTEND");
-    }
-}
-
-export class VSRepoRuntimeError extends VSRepoError {
-    constructor(message){
-        super(message, "VSREPO_RUNTIME");
-    }
-}
+// * Reexportando para se quiser importar os erros pelo arquivo principal
+export { VSRepoConfigError, VSRepoBuildError, VSRepoExtendError, VSRepoRuntimeError, VSRepoError };
 
 // * Essa é a classe pincipal, é a partir dela que serão criados os objetos dos repositories
 export class VSRepository {
