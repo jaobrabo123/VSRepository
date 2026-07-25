1361125677082ca00814e890c57eec9f0b02c550
jaobrabo123 <jp2904.lindo@gmail.com>
2026-07-25 17:58:38 -0300

    fix: adiciona validação do tipo do parâmetro 'value' de QueryMethod em tempo de execução

--- src/internal/decorators/query-method.decorator.ts
+++ src/internal/decorators/query-method.decorator.ts
@@ -1,9 +1,16 @@
 import { DINAMIC_METHODS_KEY } from "../constants/dinamic-methods-key.constant";
 import { DynamicMethodMetadata } from "../entities/dynamic-method-metadata.entity";
+import { VSRepoDecoratorError } from "../errors/vs-repo.error";
 import { validateQueryMethodOptions } from "../validation/query-method-options.validate";
 import { QueryMethodOptions } from "../validation/types/query-method-options.type";
 
-export function QueryMethod(value: string, options?: unknown): PropertyDecorator {
+export function QueryMethod(value: unknown, options?: unknown): PropertyDecorator {
+    if (typeof value !== "string") {
+        throw new VSRepoDecoratorError(
+            `[VSRepository] (unknown: decorator) 'value' must be a valid string`,
+        );
+    }
+
     const validatedConfig: QueryMethodOptions = options
         ? validateQueryMethodOptions(options)
         : { modifying: false };
