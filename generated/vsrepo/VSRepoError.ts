9db2ced64b985b6ffae69854c4268ac4b99aeeb7
jaobrabo123 <jp2904.lindo@gmail.com>
2026-07-27 00:33:19 -0300

    fix: VSRepoError agora exporta VSRepoDecoratorError no arquivo gerado

--- generated/vsrepo/VSRepoError.ts
+++ generated/vsrepo/VSRepoError.ts
@@ -11,7 +11,8 @@ import {
   VSRepoConfigError as VSRepoConfigErrorRuntime,
   VSRepoBuildError as VSRepoBuildErrorRuntime,
   VSRepoExtendError as VSRepoExtendErrorRuntime,
-  VSRepoRuntimeError as VSRepoRuntimeErrorRuntime
+  VSRepoRuntimeError as VSRepoRuntimeErrorRuntime,
+  VSRepoDecoratorError as VSRepoDecoratorErrorRuntime,
 } from 'vsrepo/VSRepoError';
 
 import type {
@@ -19,13 +20,15 @@ import type {
   VSRepoConfigError as VSRepoConfigErrorType,
   VSRepoBuildError as VSRepoBuildErrorType,
   VSRepoExtendError as VSRepoExtendErrorType,
-  VSRepoRuntimeError as VSRepoRuntimeErrorType
+  VSRepoRuntimeError as VSRepoRuntimeErrorType,
+  VSRepoDecoratorError as VSRepoDecoratorErrorType,
 } from './VSRepoError.types';
 
-export const VSRepoError: typeof VSRepoErrorType = VSRepoErrorRuntime;
-export const VSRepoConfigError: typeof VSRepoConfigErrorType = VSRepoConfigErrorRuntime;
-export const VSRepoBuildError: typeof VSRepoBuildErrorType = VSRepoBuildErrorRuntime;
-export const VSRepoExtendError: typeof VSRepoExtendErrorType = VSRepoExtendErrorRuntime;
-export const VSRepoRuntimeError: typeof VSRepoRuntimeErrorType = VSRepoRuntimeErrorRuntime;
+export const VSRepoError = VSRepoErrorRuntime as typeof VSRepoErrorType;
+export const VSRepoConfigError = VSRepoConfigErrorRuntime as typeof VSRepoConfigErrorType;
+export const VSRepoBuildError = VSRepoBuildErrorRuntime as typeof VSRepoBuildErrorType;
+export const VSRepoExtendError = VSRepoExtendErrorRuntime as typeof VSRepoExtendErrorType;
+export const VSRepoRuntimeError = VSRepoRuntimeErrorRuntime as typeof VSRepoRuntimeErrorType;
+export const VSRepoDecoratorError = VSRepoDecoratorErrorRuntime as typeof VSRepoDecoratorErrorType;
 
 export type * from './VSRepoError.types';
